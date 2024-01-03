376adf1ba095da36d5d7f6addd56b038e7aa18c7
Tyler Pierson <tylerpierson92@gmail.com>
2024-01-02 20:01:29 -0600

    Added MongoDB to server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Initial Setup
-// require('dotenv').config()
+require('dotenv').config()
 const express = require('express') // imported express from node modules
 const mongoose = require('mongoose')
 const jsxEngine = require('jsx-view-engine')
@@ -17,10 +17,10 @@ app.use(express.urlencoded({ extended:true }))
 app.set('view engine', 'jsx')
 app.engine('jsx', jsxEngine())
 
-// mongoose.connect(process.env.MONGO_URI)
-// mongoose.connection.once('open', () => {
-//     console.log('connected to mongodb')
-// })
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('connected to mongodb')
+})
 
 // Index
 
