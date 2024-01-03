0604bcb2b75732cec7360e9be116a4c24c0fb5c7
Tyler Pierson <tylerpierson92@gmail.com>
2024-01-02 20:06:18 -0600

    Created mongoose schema in models directory

--- models/log.js
+++ models/log.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const logSchema = new mongoose.Schema({
     title: { type: String, require: true },
     entry: { type: String, require: true },
-    shipIsBroken: Boolean
+    shipIsBroken: {
+        type: Boolean,
+        default: true
+    }
 })
 
 const Log = mongoose.model('Log', logSchema)
