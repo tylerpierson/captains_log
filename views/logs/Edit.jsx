de21ff2d8ede4b74ad00e0adc656001d246b4e98
Tyler Pierson <tylerpierson92@gmail.com>
2024-01-03 13:09:44 -0600

    Updated Edit.jsx and New.jsx to display a textarea rather than an input text box

--- views/logs/Edit.jsx
+++ views/logs/Edit.jsx
@@ -8,7 +8,7 @@ function Edit(props) {
             <a href="/logs">Go back to Index Page</a>
             <form action={`/logs/${_id}?_method=PUT`} method="POST">
                 Title: <input type="text" name="title" defaultValue={title}/><br/>
-                Entry: <input type="textarea" name="entry" defaultValue={entry}/><br/>
+                Entry: <textarea rows="5" cols="33" name="entry" defaultValue={entry}></textarea><br/>
                 Ship is Broken: {shipIsBroken? <input type="checkbox" name="shipIsBroken" defaultChecked/>: <input type="checkbox" name="shipIsBroken"/>}<br/>
                 <input type="submit" value="Update Entry"/>
             </form>
--- views/logs/New.jsx
+++ views/logs/New.jsx
@@ -7,7 +7,7 @@ function New(props) {
             <a href="/logs">Go back to Index Page</a>
             <form action="/logs" method="POST">
                 Title: <input type="text" name="title"/><br/>
-                Entry: <input type="textarea" name="entry"/><br/>
+                Entry: <textarea rows="5" cols="33" placeholder="Add your entry here..." name="entry"></textarea><br/>
                 Ship is Broken: <input type="checkbox" name="shipIsBroken" defaultChecked/><br/>
                 <input type="submit" value="Create Log"/>
             </form>
